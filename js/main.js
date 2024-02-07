21646e6eb0685f83ef9b8223c824b563141a6fb1
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-02-07 22:55:22 +0300

    HTML/CSS/JS owl-carousel lib added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.querySelector('.button-nav').addEventListener('click', function(){
     document.querySelector('.nav-mobile').classList.toggle('nav-mobile--active');
  });
+
+ // Вызов карусели
+$(document).ready(function(){
+    $("#slider-gifts").owlCarousel({
+        items: 1,
+        dots: false,
+        margin: 40,
+        loop: true
+    });
+  });
